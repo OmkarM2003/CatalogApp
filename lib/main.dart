8e1e277cfb47ec04ea0f93e98322597cdbb88b2d
omkar20003 <omandave2003@gmail.com>
2023-07-25 17:42:11 +0530

    Resolve conflicts in main.dart and loginpage.dart

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,17 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 import 'package:flutter_application_1/pages/homepage.dart';
 import 'package:flutter_application_1/pages/loginpage.dart';
 import 'package:google_fonts/google_fonts.dart';
+=======
+<<<<<<< Updated upstream
+=======
+import 'package:flutter_application_1/pages/homepage.dart';
+import 'package:flutter_application_1/pages/loginpage.dart';
+import 'package:flutter_application_1/utils/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
 
 void main() {
   runApp(MyApp());
@@ -21,11 +31,23 @@ class MyApp extends StatelessWidget {
       darkTheme: ThemeData(
         brightness: Brightness.dark,
       ),
+<<<<<<< Updated upstream
       initialRoute: "/login",
       routes: {
         "/": (context) => const loginpage(),
         "/login": (context) => const loginpage()
       },
+=======
+<<<<<<< Updated upstream
+=======
+      initialRoute: "/login",
+      routes: {
+        "/": (context) => const loginpage(),
+        MyRoutes.homeRoute : (context) => homepage(),
+        "/login": (context) => const loginpage()
+      },
+>>>>>>> Stashed changes
+>>>>>>> Stashed changes
     );
   }
 }
--- lib/pages/loginpage.dart
+++ lib/pages/loginpage.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+<<<<<<< Updated upstream
 
 // ignore: camel_case_types
 class loginpage extends StatelessWidget {
@@ -62,3 +63,107 @@ class loginpage extends StatelessWidget {
     );
   }
 }
+=======
+import 'package:flutter_application_1/utils/routes.dart';
+
+// ignore: camel_case_types
+class loginpage extends StatefulWidget {
+  const loginpage({Key? key}) : super(key: key);
+
+  @override
+  State<loginpage> createState() => _loginpageState();
+}
+
+class _loginpageState extends State<loginpage> {
+
+  String name = "";
+  bool changebutton = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Material(
+      color: Colors.white,
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            Image.asset("assets/imgs/login.png",
+              fit: BoxFit.cover),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Text(
+              "Welcome $name",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            const SizedBox(
+              height: 20.0,
+            ),
+            Padding(
+              padding:
+                  const EdgeInsets.symmetric(vertical: 16.0, horizontal: 32.0),
+              child: Column(
+                children: [
+                  TextFormField(
+                    decoration: InputDecoration(
+                      hintText: "Enter Username",
+                      labelText: "UserName",
+                    ),
+                    onChanged: (value) {
+                      name= value;
+                      setState(() {});
+                    },
+                  ),
+                  TextFormField(
+                    obscureText: true,
+                    decoration: InputDecoration(
+                      hintText: "Enter Password",
+                      labelText: "Password",
+                    ),
+                  ),
+                  const SizedBox(
+                    height: 20.0,
+                  ),
+                  InkWell(
+                    onTap: () async {
+                      setState(() {
+                        changebutton=true;
+                      });
+                      await Future.delayed(Duration(seconds: 1));
+                      Navigator.pushNamed(context, MyRoutes.homeRoute);
+                    },
+                    child: AnimatedContainer(
+                      duration: Duration(seconds: 1),
+                      width: changebutton? 50: 120,
+                      height: 50,
+                      alignment: Alignment.center,
+                      child: changebutton
+                      ?Icon(
+                        Icons.done, 
+                        color: Colors.white,
+                        )
+                        :Text(
+                        "Login",
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontWeight: FontWeight.bold,
+                          fontSize: 18),
+                      ),
+                      decoration: BoxDecoration(
+                        color: Colors.deepOrange,
+                        borderRadius: BorderRadius.circular(changebutton?50:8)
+                      ),
+                    ),
+                  )
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
+>>>>>>> Stashed changes
